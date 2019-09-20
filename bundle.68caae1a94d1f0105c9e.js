39dcecdf35084d9ee1c35adcb836cfde9258631d
thembones79 <michal.szumnarski@gmail.com>
2019-09-20 21:57:01 +0200

    added webpack dev server

--- bundle.68caae1a94d1f0105c9e.js
+++ bundle.68caae1a94d1f0105c9e.js
@@ -3333,6 +3333,7 @@ __webpack_require__(212);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+console.log("Hi!");
 var App = function App() {
   var store = (0, _redux.createStore)(_reducers2.default, {}, (0, _redux.applyMiddleware)(_reduxThunk2.default));
 
